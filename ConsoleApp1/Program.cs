e8818c14cd18780799199e2924215fc69f498966
ValentineCats <pyrostheotherfiremage@gmail.com>
2019-12-15 13:37:07 -0500

    Test

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using Nancy.Hosting.Self;
+using System;
+
+namespace NancyStandalone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var host = new NancyHost(new Uri("http://localhost:80")))
+            {
+                host.Start();
+
+                Console.WriteLine("NancyFX Stand alone test application.");
+                Console.WriteLine("Press enter to exit the application");
+                Console.WriteLine("HMMMMMM");
+                Console.WriteLine("HMMMMMMX2");
+                Console.ReadLine();
+            }
+        }
+    }
+}
